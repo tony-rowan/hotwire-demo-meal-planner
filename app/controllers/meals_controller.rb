5890e6d2734b3539dbab6b1557f0604f15f85a71
Tony Rowan <tony-rowan@cookpad.com>
2022-05-17 13:19:44 +0100

    Remove notices for code clarity

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,44 +1,39 @@
 class MealsController < ApplicationController
   before_action :set_meal, only: %i[edit update destroy]
 
-  # GET /meals/new
   def new
     @meal = Meal.new(date: new_meal_date)
   end
 
-  # GET /meals/1/edit
   def edit
   end
 
-  # POST /meals or /meals.json
   def create
     @meal = Meal.new(meal_params)
 
     if @meal.save
-      redirect_to root_path, notice: "Meal was successfully created."
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /meals/1 or /meals/1.json
   def update
     if @meal.update(meal_params)
-      redirect_to root_path, notice: "Meal was successfully updated."
+      redirect_to root_path
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /meals/1 or /meals/1.json
   def destroy
     @meal.destroy
 
-    redirect_to root_path, notice: "Meal was successfully destroyed."
+    redirect_to root_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_meal
       @meal = Meal.find(params[:id])
     end
@@ -47,7 +42,6 @@ class MealsController < ApplicationController
       params[:date] || Date.today
     end
 
-    # Only allow a list of trusted parameters through.
     def meal_params
       params.require(:meal).permit(:meal, :date)
     end
