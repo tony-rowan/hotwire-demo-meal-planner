f6809501c547d8151cb96481190de913965263b2
Tony Rowan <tony-rowan@cookpad.com>
2022-05-17 11:41:11 +0100

    Enable real time updates
    Subscribe to changes to meals to update the calendar in real time. This
    allows multiple users to collborate on planning their meals and see each
    others changes in real time.
    
    We can't make use of the normal helper since creating and deleting meals
    does not cause an insert or removal from the calendar list. Instead
    every change to meals causes a replace on the associated date.

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,14 @@
 class Meal < ApplicationRecord
   validates :meal, presence: true
   validates :date, presence: true
+
+  after_create_commit ->(meal) do
+    broadcast_replace_to "calendar", partial: "calendar/date", locals: { date: meal.date, meal: meal }, target: "date_#{meal.date}"
+  end
+  after_update_commit ->(meal) do
+    broadcast_replace_to "calendar", partial: "calendar/date", locals: { date: meal.date, meal: meal }, target: "date_#{meal.date}"
+  end
+  after_destroy_commit ->(meal) do
+    broadcast_replace_to "calendar", partial: "calendar/date", locals: { date: meal.date, meal: nil }, target: "date_#{meal.date}"
+  end
 end
